3c932368e2e3e33a4e83415f9526f220247f80b0
Patrick Pale <patrick.pale@atomicobject.com>
2023-12-15 13:56:03 -0500

    Day 3 part 2 complete

--- src/3/index.ts
+++ src/3/index.ts
@@ -46,6 +46,48 @@ const sumPartNumbers = (schematic: string[]): number => {
   }, 0);
 }
 
+const mapNumbersToLocations = (schematic: string[]): number[][] => {
+  const numLocs = parseNumberLocations(schematic);
+  const schematicMap: number[][] = new Array(schematic.length).fill(false).map(() => new Array(schematic[0].length).fill(0));
+  numLocs.forEach(num => {
+    num.number.split('').forEach((_, idx) => {
+      schematicMap[num.yStart][num.xStart + idx] = parseInt(num.number, 10);
+    });
+  });
+  return schematicMap;
+}
+
+const sumGearRatio = (numMap: number[][], x: number, y: number): number => {
+  const xMax = numMap[0].length;
+  const yMax = numMap.length - 1;
+  const [xStart, yStart] = [Math.max(x-1, 0), Math.max(y-1, 0)];
+  const [xEnd, yEnd] = [Math.min(x+1, xMax), Math.min(y+1, yMax)];
+  const ratios = [
+    numMap[yStart][xStart], numMap[yStart][x], numMap[yStart][xEnd],
+    numMap[y][xStart], numMap[y][x], numMap[y][xEnd],
+    numMap[yEnd][xStart], numMap[yEnd][x], numMap[yEnd][xEnd]
+  ];
+  const uniques = new Set(ratios);
+  uniques.delete(0);
+  if (uniques.size === 2) {
+    const values = [...uniques];
+    return values[0] * values[1];
+  }
+  return 0;
+}
+
+const findGearRatios = (schematic: string[]): number => {
+  const numMap = mapNumbersToLocations(schematic);
+  return schematic.reduce((prev, curr, yIndex) => {
+    return prev + curr.split('').reduce((linePrev, lineCurr, xIndex) => {
+      if (lineCurr === '*') {
+        return linePrev + sumGearRatio(numMap, xIndex, yIndex);
+      }
+      return linePrev;
+    }, 0);
+  }, 0);
+}
+
 const testInput = [
   '467..114..',
   '...*......',
@@ -61,7 +103,8 @@ const testInput = [
 
 export const day3 = () => {
   console.log('day 3');
-  console.log(`part 1 sum: ${sumPartNumbers(input)}`);
+  console.log(`part 1 sum: ${sumPartNumbers(input)}`); // should be 525181
+  console.log(`part 2 sum: ${findGearRatios(input)}`); // should be 84289137
 }
 
 day3();
